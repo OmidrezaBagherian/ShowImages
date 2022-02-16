c087ade228db4944cb8c871cc08ab7e0fe3ecd72
OmidrezaBagherian <OmidCoder@gmail.com>
2022-02-16 07:18:57 -0800

    End project

---
+++ app/src/main/java/ir/omidrezabagherian/showimage/MainActivity.kt
@@ -0,0 +1,50 @@
+package ir.omidrezabagherian.showimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import ir.omidrezabagherian.showimage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val circularPresentDrawable = CircularProgressDrawable(this)
+        circularPresentDrawable.strokeWidth = 10f
+        circularPresentDrawable.centerRadius = 50f
+        circularPresentDrawable.start()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        Glide.with(this)
+            .load("https://picsum.photos/2000?rand=")
+            .centerCrop()
+            .placeholder(circularPresentDrawable)
+            .into(binding.imageViewOne)
+
+        Glide.with(this)
+            .load("https://picsum.photos/3000?rand=")
+            .centerCrop()
+            .placeholder(circularPresentDrawable)
+            .into(binding.imageViewTwo)
+
+        Glide.with(this)
+            .load("https://picsum.photos/4000?rand=")
+            .centerCrop()
+            .placeholder(circularPresentDrawable)
+            .into(binding.imageViewThree)
+
+        Glide.with(this)
+            .load("https://picsum.photos/5000?rand=")
+            .centerCrop()
+            .placeholder(circularPresentDrawable)
+            .into(binding.imageViewFour)
+
+        setContentView(binding.root)
+
+    }
+}
